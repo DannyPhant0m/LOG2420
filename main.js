a40e5f076b895d77247913c3e34008a9923e3d86
Danny Tran <danny.tran@polymtl.ca>
2020-02-18 00:44:22 -0500

    dsds

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ function swapImageCircle(id,primary,secondary) {
     src=document.getElementById(id).src;
     if (src.match(primary)) {
         document.getElementById(id).src=secondary;
+
+        return true;
     } else {
+      
         document.getElementById(id).src=primary;
+        return false;
     }
 }
 
@@ -17,3 +21,12 @@ function swapImageBox(id, primary, secondary) {
     }
 }
 
+function swapText(id,primary,secondary){
+
+    value = document.getElementById(id).value;
+    if (src.match(primary)) {
+        document.getElementById(id).value = secondary;
+    } else {
+        document.getElementById(id).value = primary;
+    }
+}
